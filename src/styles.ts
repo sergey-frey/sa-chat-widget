f0a43f37304013d0ae590fe797b9e56fcba0f455
Sergey Frey <sergey.frey.work@gmail.com>
2026-05-16 23:28:48 +0500

    refactor: update styles aggregator to use module.scss

--- src/styles.ts
+++ src/styles.ts
@@ -1,6 +1,5 @@
-const cssFiles = import.meta.glob("./**/*.css", { query: "?inline", eager: true });
-const scssFiles = import.meta.glob("./**/*.scss", { query: "?inline", eager: true });
+const scssFiles = import.meta.glob("./**/*.module.scss", { query: "?inline", eager: true });
 
-export default [...Object.values(cssFiles), ...Object.values(scssFiles)]
+export default [...Object.values(scssFiles)]
   .map((m) => (m as { default: string }).default)
   .join("\n");
